37bb5ae213b427388cd9eb8df4db57782ec3fd87
Maximilian Royal-Eisenberg <maxroyal.eisenberg@gmail.com>
2023-05-16 23:29:38 -0700

    updated database,js and install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,16 +15,30 @@ const initdb = async () =>
 //logic that accepts content and adds it to the database
 export const putDb = async (content)  => {
   console.log('Put to the database');
-
   const codeDb = await openDB('jate', 1);
   const tx = codeDb.transaction('jate', 'readwrite');
+  //jate store
   const store = tx.objectStore('jate');
-  //use .put() method on the 'code' and pass in the content.
+  //use .put() method on the 'store' and pass in content.
   const request = store.put({ id: 1, text: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+//logic that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  //opens database and version we want to use.
+  const codeDb = await openDB('jate', 1);
+  //new transaction, database name and privileges
+  const tx = codeDb.transaction('jate', 'readonly');
+  //jate store
+  const store = tx.objectStore('jate');
+  //use .getAll() method to get all data in the database
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+//event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    //store event
+    window.deferredPrompt = event;
+    //remove hidden class from button so it can be seen
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//event handler on `butInstall'
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+     return;
+    }
+    //prompt appears
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    //applies hidden class to button
+    butInstall.classList.toggle('hidden', true);
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+});
+
+//event handler for `appinstalled`
+window.addEventListener('appinstalled', (event) => {
+    //prompt removed
+    window.deferredPrompt = null;
+});
